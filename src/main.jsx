85cdcc8ad24cbd5587a6a4b46f0851f2c823ce7e
110819016 <71056572+110819016@users.noreply.github.com>
2023-02-16 16:10:46 +0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import './App.css' 
+// import 'https://use.fontawesome.com/releases/v5.3.1/css/all.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
